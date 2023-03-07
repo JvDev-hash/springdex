8938caf420d3039aff01f212199950505bc1877e
João Victor <jvbaprof@gmail.com>
2023-03-07 16:23:58 -0300

    initi

---
+++ src/main/java/poke/dex/springdex/SpringdexApplication.java
@@ -0,0 +1,13 @@
+package poke.dex.springdex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdexApplication.class, args);
+	}
+
+}
